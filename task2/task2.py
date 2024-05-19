f56b069726dd8bd3a0dc0ab6aa5c98f237e3e7ff
orca131 <throughtheuniverse@vivaldi.net>
2024-05-20 02:28:18 +0300

    almost there

--- task2/task2.py
+++ task2/task2.py
@@ -1,10 +1,19 @@
-#import sys
+import sys
+
+src_circle = sys.argv[1]
+src_points = sys.argv[2]
+
+with open(src_circle, 'r') as file:
+    def_circle = file.readlines()
+
+stripped = [j.strip().split(' ') for j in def_circle[:-1]]
+
+circle_x = float(stripped[0][0])
+circle_y = float(stripped[0][1])
+circle_r = float(def_circle[-1])
 
 point_y = 6
 point_x = 6
-circle_x = 1
-circle_y = 1
-circle_r = 5
 
 sq_length_x = (point_x - circle_x) ** 2
 sq_length_y = (point_y - circle_y) ** 2
