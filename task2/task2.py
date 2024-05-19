04c1e5e40aea15be2fc3736c7da4200bc3693bd2
orca131 <throughtheuniverse@vivaldi.net>
2024-05-20 02:47:18 +0300

    task2-0.2

--- task2/task2.py
+++ task2/task2.py
@@ -1,26 +1,46 @@
 import sys
 
+# Получить файлы с данными в качестве аргументов
+
 src_circle = sys.argv[1]
 src_points = sys.argv[2]
 
+# Получить параметры окружности
+
 with open(src_circle, 'r') as file:
-    def_circle = file.readlines()
+    raw_circle = file.readlines()
 
-stripped = [j.strip().split(' ') for j in def_circle[:-1]]
+# Записать координаты центра окружности в список
 
-circle_x = float(stripped[0][0])
-circle_y = float(stripped[0][1])
-circle_r = float(def_circle[-1])
+mid_circle = [j.strip().split(' ') for j in raw_circle[:-1]]
 
-point_y = 6
-point_x = 6
+# Присвоить значения переменным, описывающим окружность
 
-sq_length_x = (point_x - circle_x) ** 2
-sq_length_y = (point_y - circle_y) ** 2
+circle_x = float(mid_circle[0][0])
+circle_y = float(mid_circle[0][1])
+circle_r = float(raw_circle[-1])
 
-if sq_length_x + sq_length_y == circle_r ** 2:
-    print(0)
-elif sq_length_x + sq_length_y < circle_r ** 2:
-    print(1)
-else:
-    print(2)
+# Получить координаты
+
+with open(src_points, 'r') as file:
+    raw_points = file.readlines()
+
+# Записать координаты в список
+
+all_points = [k.strip().split(' ') for k in raw_points]
+
+# Определить положение точки относительно окружности
+# для каждой пары координат: 
+# 0 - точка лежит на окружности 
+# 1 - точка внутри
+# 2 - точка снаружи
+
+for p in all_points:
+    sq_length_x = (float(p[0]) - circle_x) ** 2
+    sq_length_y = (float(p[1]) - circle_y) ** 2
+    if sq_length_x + sq_length_y == circle_r ** 2:
+        print(0)
+    elif sq_length_x + sq_length_y < circle_r ** 2:
+        print(1)
+    else:
+        print(2)
